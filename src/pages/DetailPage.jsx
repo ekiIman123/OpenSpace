ba67f89d59516983db589878a71eaaa051a860a7
Eki Iman Jordiansyah <ekiimanjordiansyah212@gmail.com>
2024-08-15 01:08:19 +0700

    integrating redux management state to react component

---
+++ src/pages/DetailPage.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import TalkDetail from "../components/TalkDetail";
+import TalkItem from "../components/TalkItem";
+import TalkReplyInput from "../components/TalkReplyInput";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  asyncReceiveTalkDetail,
+  asyncToogleLikeTalkDetail,
+} from "../states/talkDetail/action";
+import { asyncAddTalk } from "../states/talks/action";
+
+function DetailPage() {
+  const { id } = useParams();
+  const authUser = useSelector((state) => state.authUser);
+  const talkDetail = useSelector((state) => state.talkDetail);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(asyncReceiveTalkDetail(id));
+  }, [id, dispatch]);
+
+  const onLikeTalk = () => {
+    dispatch(asyncToogleLikeTalkDetail());
+  };
+
+  const onReplyTalk = (text) => {
+    dispatch(asyncAddTalk({ text, replyTo: id }));
+  };
+
+  if (!talkDetail) {
+    return null;
+  }
+
+  return (
+    <section className="detail-page">
+      {talkDetail.parent && (
+        <div className="detail-page__parent">
+          <h3>Replying To</h3>
+          <TalkItem {...talkDetail.parent} authUser={authUser.id} />
+        </div>
+      )}
+      <TalkDetail
+        {...talkDetail}
+        authUser={authUser.id}
+        likeTalk={onLikeTalk}
+      />
+      <TalkReplyInput replyTalk={onReplyTalk} />
+    </section>
+  );
+}
+
+export default DetailPage;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from "react";
+import TalkInput from "../components/TalkInput";
+import TalksList from "../components/TalksList";
+import { useDispatch, useSelector } from "react-redux";
+import { asyncPopulateUsersAndTalks } from "../states/shared/action";
+import { asyncAddTalk, asyncToggleLikeTalk } from "../states/talks/action";
+
+function HomePage() {
+  const authUser = useSelector((state) => state.authUser);
+  const talks = useSelector((state) => state.talks);
+  const users = useSelector((state) => state.users);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(asyncPopulateUsersAndTalks());
+  }, [dispatch]);
+
+  const onAddTalk = (text) => {
+    dispatch(asyncAddTalk({ text }));
+  };
+
+  const onLike = (id) => {
+    dispatch(asyncToggleLikeTalk(id));
+  };
+
+  const talkList = talks.map((talk) => ({
+    ...talk,
+    user: users.find((user) => user.id === talk.user),
+    authUser: authUser.id,
+  }));
+
+  return (
+    <section className="home-page">
+      <TalkInput addTalk={onAddTalk} />
+      <TalksList talks={talkList} like={onLike} />
+    </section>
+  );
+}
+
+export default HomePage;
